1e8e801996970e3b53cb40c5ec5c4fa0e1bfc308
Denis Kliem <denis_kliem@yahoo.de>
2020-08-17 13:08:57 +0200

    edited assemblyinfo

--- TalesOfCrestoriaDCBot/Properties/AssemblyInfo.cs
+++ TalesOfCrestoriaDCBot/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("TaleOfCrestoria")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TalesOfCrestoriaDCBot")]
+[assembly: AssemblyDescription("A Discord Bot with Fetures for the Mobile Game Tales of Crestoria")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TaleOfCrestoria")]
+[assembly: AssemblyCompany("DjNemas")]
+[assembly: AssemblyProduct("TalesOfCrestoriaDCBot")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
